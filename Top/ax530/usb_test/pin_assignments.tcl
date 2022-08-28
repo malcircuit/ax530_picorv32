afdb94c2a2b2086770aa1d0b63601e91a20ebea1
Mallory Sutter <sir.oslay@gmail.com>
2022-08-27 23:07:16 -0500

    Blank slate the usb_test RTL

--- Top/ax530/usb_test/pin_assignments.tcl
+++ Top/ax530/usb_test/pin_assignments.tcl
@@ -2,6 +2,7 @@ set_global_assignment -name SDC_FILE ../../../Top/ax530/usb_test/usb_test.sdc
 
 set_location_assignment PIN_T21 -to clk
 set_location_assignment PIN_B19 -to reset_n
+set_location_assignment PIN_H16 -to usb_ifclk
 set_location_assignment PIN_L21 -to usb_fd[15]
 set_location_assignment PIN_L22 -to usb_fd[14]
 set_location_assignment PIN_K19 -to usb_fd[13]
@@ -23,12 +24,14 @@ set_location_assignment PIN_H19 -to usb_fifoaddr[0]
 set_location_assignment PIN_F22 -to usb_flaga
 set_location_assignment PIN_F21 -to usb_flagb
 set_location_assignment PIN_G18 -to usb_flagc
-set_location_assignment PIN_H20 -to usb_slcs
+set_location_assignment PIN_H20 -to usb_flagd
 set_location_assignment PIN_H18 -to usb_sloe
 set_location_assignment PIN_J17 -to usb_slrd
 set_location_assignment PIN_K17 -to usb_slwr
+set_location_assignment PIN_H21 -to usb_pktend
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to clk
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to reset_n
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_ifclk
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fd[15]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fd[14]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fd[13]
@@ -50,41 +53,53 @@ set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fifoaddr[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flaga
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagb
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagc
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_slcs
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagd
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_sloe
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_slrd
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_slwr
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_pktend
 
-set_location_assignment PIN_T3 -to clk_dup
-set_location_assignment PIN_U2 -to usb_flaga_dup
-set_location_assignment PIN_V1 -to usb_flagb_dup
+set_location_assignment PIN_Y2 -to usb_ifclk_dup
+set_location_assignment PIN_V4 -to usb_flaga_dup
+set_location_assignment PIN_R6 -to usb_flagb_dup
 set_location_assignment PIN_V2 -to usb_flagc_dup
-set_location_assignment PIN_R6 -to usb_sloe_dup
-set_location_assignment PIN_Y1 -to usb_slrd_dup
-set_location_assignment PIN_V4 -to usb_slwr_dup
-set_location_assignment PIN_Y2 -to usb_fd_dup[0]
-set_location_assignment PIN_N7 -to usb_fd_dup[1]
-set_location_assignment PIN_N6 -to usb_fd_dup[2]
-set_location_assignment PIN_R7 -to usb_fd_dup[3]
-set_location_assignment PIN_P6 -to usb_fd_dup[4]
-set_location_assignment PIN_P1 -to usb_fd_dup[5]
-set_location_assignment PIN_P3 -to usb_fd_dup[6]
-set_location_assignment PIN_R2 -to usb_fd_dup[7]
-set_location_assignment PIN_P5 -to usb_fd_dup[8]
-set_location_assignment PIN_AA1 -to usb_fd_dup[9]
-set_location_assignment PIN_V3 -to usb_fd_dup[10]
-set_location_assignment PIN_W2 -to usb_fd_dup[11]
-set_location_assignment PIN_W1 -to usb_fd_dup[12]
-set_location_assignment PIN_T5 -to usb_fd_dup[13]
-set_location_assignment PIN_T4 -to usb_fd_dup[14]
-set_location_assignment PIN_U1 -to usb_fd_dup[15]
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to clk_dup
+set_location_assignment PIN_V1 -to usb_flagd_dup
+set_location_assignment PIN_U2 -to usb_slwr_dup
+set_location_assignment PIN_T3 -to usb_slrd_dup
+set_location_assignment PIN_P5 -to usb_sloe_dup
+set_location_assignment PIN_R2 -to usb_pktend_dup
+set_location_assignment PIN_P3 -to usb_fifoaddr_dup[0]
+set_location_assignment PIN_P1 -to usb_fifoaddr_dup[1]
+
+set_location_assignment PIN_AA1 -to usb_fd_dup[0]
+set_location_assignment PIN_V3 -to usb_fd_dup[1]
+set_location_assignment PIN_W2 -to usb_fd_dup[2]
+set_location_assignment PIN_W1 -to usb_fd_dup[3]
+set_location_assignment PIN_T5 -to usb_fd_dup[4]
+set_location_assignment PIN_T4 -to usb_fd_dup[5]
+set_location_assignment PIN_U1 -to usb_fd_dup[6]
+set_location_assignment PIN_R5 -to usb_fd_dup[7]
+set_location_assignment PIN_P4 -to usb_fd_dup[8]
+set_location_assignment PIN_R1 -to usb_fd_dup[9]
+set_location_assignment PIN_P2 -to usb_fd_dup[10]
+set_location_assignment PIN_T7 -to usb_fd_dup[11]
+set_location_assignment PIN_N5 -to usb_fd_dup[12]
+set_location_assignment PIN_M5 -to usb_fd_dup[13]
+set_location_assignment PIN_P7 -to usb_fd_dup[14]
+set_location_assignment PIN_M7 -to usb_fd_dup[15]
+
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_ifclk_dup
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_slwr_dup
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_slrd_dup
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_sloe_dup
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagc_dup
-set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagb_dup
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flaga_dup
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagb_dup
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagc_dup
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_flagd_dup
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_pktend_dup
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fifoaddr_dup[1]
+set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fifoaddr_dup[0]
+
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fd_dup[0]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fd_dup[1]
 set_instance_assignment -name IO_STANDARD "3.3-V LVTTL" -to usb_fd_dup[2]
--- hdl/src/usb_test.v
+++ hdl/src/usb_test.v
@@ -13,27 +13,36 @@ module usb_test(
         input  wire         clk,            //! Clock (50 MHz)
         input  wire         reset_n,        //! Reset 
         
-        input  wire         usb_flaga,      //! CY68013 EP2 FIFO empty flag; 1:not empty; 0: empty
-        input  wire         usb_flagb,      //! CY68013 EP4 FIFO empty flag; 1:not empty; 0: empty
-        input  wire         usb_flagc,      //! CY68013 EP6 FIFO full flag; 1:not full; 0: full
-        output reg          usb_slcs,       //! CY68013 chip select (active low)
+        input  wire         usb_ifclk,      //! CY68013 interface clock
+
+        input  wire         usb_flaga,      //! CY68013 FIFO programmable flag (default bahavior, except for EP4)
+        input  wire         usb_flagb,      //! CY68013 FIFO full flag (active low)
+        input  wire         usb_flagc,      //! CY68013 FIFO empty flag (active low)
+        input  wire         usb_flagd,      //! CY68013 EP4 programmable flag (active low when FIFO byte count >= 1)
+
         output reg          usb_slrd,       //! CY68013 read control (active low)
         output reg          usb_slwr,       //! CY68013 write control (active low)
         output reg          usb_sloe,       //! CY68013 data output enable (active low)
-        output reg   [1:0]  usb_fifoaddr,   //! CY68013 FIFO Address
+        output reg          usb_pktend,     //! CY68013 packet end marker (active low - only used when sending data to the host)
+
+        output reg   [1:0]  usb_fifoaddr,   //! CY68013 FIFO Address - 2'b00: EP2 (from host), 2'b01: EP4 (from host), 2'b10: EP6 (to host), 2'b11: EP8 (to host)
         inout  wire [15:0]  usb_fd,         //! CY68013 FIFO data bus
 
-        output reg clk_dup,             //! Duplicate for debugging
-        output reg usb_sloe_dup,        //! Duplicate for debugging
-        output reg usb_slrd_dup,        //! Duplicate for debugging
-        output reg usb_slwr_dup,        //! Duplicate for debugging
-        output wire [15:0] usb_fd_dup,  //! Duplicate for debugging
-        output reg usb_flaga_dup,       //! Duplicate for debugging
-        output reg usb_flagb_dup,       //! Duplicate for debugging
-        output reg usb_flagc_dup,       //! Duplicate for debugging
+        output reg usb_ifclk_dup,   //! Duplicate for debugging
+        output reg usb_flaga_dup,   //! Duplicate for debugging
+        output reg usb_flagb_dup,   //! Duplicate for debugging
+        output reg usb_flagc_dup,   //! Duplicate for debugging
+        output reg usb_flagd_dup,   //! Duplicate for debugging
+        output reg usb_sloe_dup,    //! Duplicate for debugging
+        output reg usb_slrd_dup,    //! Duplicate for debugging
+        output reg usb_slwr_dup,    //! Duplicate for debugging
+        output reg usb_pktend_dup,  //! Duplicate for debugging
+
+        output wire [ 1:0] usb_fifoaddr_dup,    //! Duplicate for debugging
+        output wire [15:0] usb_fd_dup,          //! Duplicate for debugging
 
         output wire [7:0] SMG_Data,     //! Seven segment LED signals (active low)
-        output wire [5:0] Scan_Sig     //! Determines which segment is active (active low)
+        output wire [5:0] Scan_Sig      //! Determines which segment is active (active low)
     );
 
     genvar n;
@@ -46,13 +55,15 @@ module usb_test(
 
     always @*
     begin : duplicate_signals
-        clk_dup = clk;
-        usb_sloe_dup = usb_sloe;
-        usb_slrd_dup = usb_slrd;
-        usb_slwr_dup = usb_slwr;
+        usb_ifclk_dup = usb_ifclk;
         usb_flaga_dup = usb_flaga;
         usb_flagb_dup = usb_flagb;
         usb_flagc_dup = usb_flagc;
+        usb_flagd_dup = usb_flagd;
+        usb_sloe_dup  = usb_sloe;
+        usb_slrd_dup  = usb_slrd;
+        usb_slwr_dup  = usb_slwr;
+        usb_pktend_dup = usb_pktend;
     end
 
     reg  [15:0] fifo_in;
@@ -91,170 +102,7 @@ module usb_test(
     );
 
     reg [15:0] data_reg;    //! Temporary read/write data storage
-
-    reg bus_busy;   //! Active high when the module is sending or receiving data
-    wire access_req = usb_flaga & usb_flagc & (bus_busy == 1'b0); //! Active high when it's possible to read/write from/to the FIFO
-    reg usb_fd_en;  //! Tri-state output enable for `usb_fd`
-
-    // Possible states
-    `define IDLE		6'b000001
-    `define EP2_RD_CMD	6'b000010
-    `define EP2_RD_DATA 6'b000100
-    `define EP2_RD_OVER 6'b001000
-    `define EP6_WR_CMD  6'b010000
-    `define EP6_WR_OVER 6'b100000
-
-    reg [5:0] usb_state;    //! State machine register
-    reg [4:0] delay_count;  //! Used to delay state changes
-    
-    always @(negedge clk or negedge reset_n)
-    begin : fifo_rd_wr_state_machine
-        if (~reset_n)
-        begin
-            fifo_wr <= 1'b0;
-            rx_count <= 24'h000000;
-        end
-        else
-        begin
-            case(usb_state)
-                `EP2_RD_DATA:
-                begin      //EP2 FIFO Read Data
-                    if(delay_count == 8)
-                        fifo_wr <= 1'b1;
-                end
-                `EP2_RD_OVER:
-                begin
-                    if(delay_count == 4)
-                    begin
-                        fifo_wr <= 1'b0;
-                        rx_count <= {16'h0000, fifo_count};
-                    end
-                end
-            endcase
-        end
-    end
-
-    always @(posedge clk or negedge reset_n)
-    begin : rd_wr_state_machine
-        if (~reset_n)
-        begin
-            usb_fifoaddr <= 2'b00;
-            usb_slcs <= 1'b0;
-            usb_sloe <= 1'b1;
-            usb_slrd <= 1'b1;
-            usb_slwr <= 1'b1;
-            usb_fd_en <= 1'b0;
-            usb_state <= `IDLE;
-        end
-        else
-        begin
-            case(usb_state)
-                `IDLE:
-                begin
-                    usb_fifoaddr <= 2'b00;
-                    delay_count <= 0;
-                    usb_fd_en <= 1'b0;
-                    if (access_req == 1'b1)
-                    begin
-                        usb_state <= `EP2_RD_CMD;
-                        bus_busy <= 1'b1;
-                    end
-                    else
-                    begin
-                        bus_busy <= 1'b0;
-                        usb_state <= `IDLE;
-                    end
-                end
-                `EP2_RD_CMD:
-                begin      //EP2 FIFO Read Command
-                    if(delay_count == 2)
-                    begin
-                        usb_slrd <= 1'b1;
-                        usb_sloe <= 1'b0;
-                        delay_count <= delay_count + 1'b1;
-                    end
-                    else if(delay_count == 8)
-                    begin
-                        usb_slrd <= 1'b0;
-                        usb_sloe <= 1'b0;
-                        delay_count <= 0;
-                        usb_state <= `EP2_RD_DATA;
-                    end
-                    else
-                    begin
-                        delay_count <= delay_count + 1'b1;
-                    end
-                end
-                `EP2_RD_DATA:
-                begin      //EP2 FIFO Read Data
-                    if(delay_count == 8)
-                    begin
-                        usb_slrd <= 1'b1;
-                        usb_sloe <= 1'b0;
-                        delay_count <= 0;
-                        usb_state <= `EP2_RD_OVER;
-                        data_reg <= usb_fd;
-
-                        fifo_in <= usb_fd;
-                    end
-                    else
-                    begin
-                        usb_slrd <= 1'b0;
-                        usb_sloe <= 1'b0;
-                        delay_count <= delay_count + 1'b1;
-                    end
-                end
-                `EP2_RD_OVER:
-                begin
-                    if(delay_count == 4)
-                    begin
-                        usb_slrd <= 1'b1;
-                        usb_sloe <= 1'b1;
-                        delay_count <= 0;
-                        usb_fifoaddr <= 2'b10;
-                        usb_state <= `EP6_WR_CMD;
-                    end
-                    else
-                    begin
-                        usb_slrd <= 1'b1;
-                        usb_sloe <= 1'b0;
-                        delay_count <= delay_count + 1'b1;
-                    end
-                end
-                `EP6_WR_CMD:
-                begin
-                    if(delay_count == 8)
-                    begin
-                        usb_slwr <= 1'b1;
-                        delay_count <= 0;
-                        usb_state <= `EP6_WR_OVER;
-                    end
-                    else
-                    begin
-                        usb_slwr <= 1'b0;
-                        usb_fd_en <= 1'b1;
-                        delay_count <= delay_count + 1'b1;
-                    end
-                end
-                `EP6_WR_OVER:
-                begin
-                    if(delay_count == 4)
-                    begin
-                        usb_fd_en <= 1'b0;
-                        bus_busy <= 1'b0;
-                        delay_count <= 0;
-                        usb_state <= `IDLE;
-                    end
-                    else
-                    begin
-                        delay_count <= delay_count + 1'b1;
-                    end
-                end
-                default:
-                    usb_state <= `IDLE;
-            endcase
-        end
-    end
+    reg usb_fd_en;          //! Tri-state output enable for `usb_fd`
 
     assign usb_fd = usb_fd_en ? data_reg : 16'bz;
 
